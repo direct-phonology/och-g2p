0bec17b99b5bbc255740ff30b4fbe39f22e8cfd4
Nick Budak <thatbudakguy@gmail.com>
2022-02-22 20:02:58 -0800

    Support wandb sweeps with extra code loading

--- scripts/sweep.py
+++ scripts/sweep.py
@@ -1,14 +1,21 @@
-import typer
 from pathlib import Path
-from spacy.training.loop import train
-from spacy.training.initialize import init_nlp
-from spacy import util
-from thinc.api import Config
+from typing import Optional
+
+import typer
 import wandb
+from spacy import util
+from spacy.cli import _util
+from spacy.training.initialize import init_nlp
+from spacy.training.loop import train
+from thinc.api import Config
 
 
-def main(default_config: Path, output_path: Path):
+def main(default_config: Path, output_path: Path, code: Optional[Path] = None):
     loaded_local_config = util.load_config(default_config)
+
+    # Allow importing from an external script, to load the Phonologizer
+    _util.import_code(code)
+    
     with wandb.init() as run:
         sweeps_config = Config(util.dot_to_dict(run.config))
         merged_config = Config(loaded_local_config).merge(sweeps_config)
